0fd5fa12bf2ce72de88b18dd95fd05edcb4b4736
Ruter <ruter.lv@gmail.com>
2018-05-10 12:05:48 +0800

    rm

--- src/libs/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var payUrl = "https://pay.nebulas.io/api/pay";
-
-module.exports = {
-    payUrl: payUrl
-};
--- src/vendors.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue';
-import iView from 'iview';
-import VueRouter from 'vue-router';
-import axios from 'axios';
-import {Neb, HttpRequest, Account} from 'nebulas';
-import NebPay from './libs/nebpay';
